88a5afb13677a0e1894b7ffe85a998f0efe7967e
itsabttime <117502862+itsabttime@users.noreply.github.com>
2023-04-01 11:40:44 +0530

    Add files via upload

---
+++ final.bat
@@ -0,0 +1,88 @@
+set /a randum=%random%
+wmic nic get netconnectionid,netconnectionstatus,netenabled /format:CSV >> CSV.NET_ID.NETCONN.%randum%.CSV
+type CSV.NET_ID.NETCONN.%randum%.CSV | find /v "NetConnectionID,NetConnectionStatus,NetEnabled" >> CSV.NET_ID.NETCONN.%randum%2.CSV
+del CSV.NET_ID.NETCONN.%randum%.CSV 
+ECHO OFF
+CLS
+setlocal enabledelayedexpansion
+set list=
+set /a counter=0
+
+for /f "delims=" %%i in (CSV.NET_ID.NETCONN.%randum%2.CSV) do for /f "tokens=1,2,3,4 delims=," %%a in ("%%i") do if "%%b" NEQ "" set interface[!counter!]=%%b&set /a counter+=1
+REM echo. There are !counter! results. Here are the names-
+set /a counter=counter-1
+for /l %%i in (0,1,!counter!) do set temp=!interface[%%i]!&set ptr[%%i]=!temp:^&amp^;=^&!
+
+for /l %%i in (0,1,!counter!) do echo %%i.!ptr[%%i]!
+del CSV.NET_ID.NETCONN.%randum%2.CSV
+
+:tryagain
+set /a x=1
+set /p delet=
+set delet="%delet%"
+set delet=%delet:^=^^%
+set delet=%delet:^=^^^=%
+set delet=%delet:&=^&%
+set delet=%delet:`=``%
+set delet=%delet:>=^>%
+set delet=%delet:\=\\"%
+set delet=%delet:"=%
+
+if "!delet!"=="" (ECHO EMPTY STRING& GOTO tryAGAIN)
+
+for /f %%i in ('powershell -c "\""%delet%"\" -match '^[0-9]$'"') do set stat=%%i
+
+
+if "%stat%"=="True" (Echo.) Else (Echo.Please Enter A Choice.&Goto tryagain)
+
+for /f %%i in ('powershell -c "$str=%delet%;write-host $str"') do set number=%%i
+if !number! LEQ !counter! (echo.GoodChoice 1>NUL) else (echo.Invalid Number&goto tryagain)
+set interface2day=!ptr[%number%]!
+echo.Chosen Interface !!interface2day!!
+EndLocal
+setlocal Enabledelayedexpansion
+
+:again
+echo.^(1^)Disable
+echo.^(2^)Enable
+
+set /a x=1
+set /p delet=
+set delet="%delet%"
+
+set delet=%delet:^=^^%
+
+set delet=%delet:^=^^^=%
+
+set delet=%delet:&=^&%
+
+set delet=%delet:`=``%
+
+set delet=%delet:>=^>%
+
+set delet=%delet:\=\\"%
+
+set delet=%delet:"=%
+
+
+if "!delet!"=="" (ECHO EMPTY STRING& GOTO AGAIN)
+
+for /f %%i in ('powershell -c "\""%delet%"\" -match '^[0-9]$'"') do set stat=%%i
+
+
+if "%stat%"=="True" (Echo.) Else (Echo.Please Enter A Choice.&Goto again)
+
+for /f %%i in ('powershell -c "$str=%delet%;write-host $str"') do set number=%%i
+if !number!==1 (goto disable)
+if !number!==2 (goto enable) else (goto Invalid Choice&goto again)
+:disable
+echo.Disabling..
+netsh interface set interface "%interface2day%" disable
+goto eof
+Rem.netsh interface set interface "%interface2day%" disable 2>&1>con
+:enable
+echo.Enabling...
+netsh interface set interface "%interface2day%" enable
+
+:eof
+Pause >nul

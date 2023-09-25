0997d13f40c9560f8b2de4c7e5cfb4e408487d70
ITS0ABOUT0TIME <117502862+ITS0ABOUT0TIME@users.noreply.github.com>
2023-09-25 23:53:30 +0530

    Update final.bat

--- final.bat
+++ final.bat
@@ -1,9 +1,15 @@
-BCDEDIT /ENUM 2>nul 1>nul
-cls
+REM BCDEDIT /ENUM 2>nul 1>nul
+REM cls
 REM I AM A CHRISTIAN 
 REM ****************
-IF %ERRORLEVEL%==1 TITLE NOT ADMINISTRATOR DETECTED.&echo off&cls&ECHO IT SEEMS U R NOT RUNNING AS ADMINISTRATOR :) zz&echo. please RUN as Administrator &&echo. ----------------------------------------------------------------------------------&echo.^|This program cannot make changes to the Network without Administrative Priveleges ^|&echo. ----------------------------------------------------------------------------------&echo.&timeout 1 >nul&echo (right-click bat file and select "Run as Administrator)"&echo.Press any key to continue without Administrative priveleges&pause >NUL
-set /a helper_interval=60
+
+REM First
+echo off
+BCDEDIT /ENUM 2>nul 1>nul
+cls
+IF %ERRORLEVEL%==0 echo off& cls&title Main&echo.------------Welcome!---------------&echo.            to&echo.              Net Disabler&timeout 2 >NUL
+IF %ERRORLEVEL%==1 TITLE User Mode.&echo off&cls&echo. ----------------------------------------------------------------------------------&echo.^|This script cannot make changes to the Network without Administrative Priveleges ^|&echo. ----------------------------------------------------------------------------------&echo.&echo.What can you do:&echo.Run this script with administrative priveleges Next time ;)&timeout 1 >nul&echo (right-click bat file and select "Run as Administrator)"&echo.Press any key to continue running anyway&pause >NUL
+set /a helper_interval=1600
 set /a break_interval=2
 REM set break_interval=0 to skip early messages..
 REM If helper is not clearing up files/cleaning
@@ -13,45 +19,58 @@ REM In Milliseconds
 @echo off 
 title Starting...
 mode 30,20
-echo.ATTENTION !!!!!!!!
-echo.~~
-echo.i am 
-echo.initiating 
+echo.=== ATTENTION ===
 echo.
-echo. a
-echo.~FILE CLEANUP SEQUENCE~
-echo.-:JUNK FILE DELETE:-
+echo./* initiating
+echo.   junk clean-up ! 
+echo.
+echo. 
+echo.  Helper files
+echo.need to be cleaned.
+echo.
 echo. 
 echo. using a helper window
 echo.~~
-echo.PLEASE INCREASE %%HELPER_INTERVAL%% IF 
-ECHO.THIS DOES NOT HELP
-
+echo.--------------------------
+ECHO.if files are not removed
+echo.increase %%helper_interval%% 
+echo.variable
+echo. --------------------------
+echo.Helper 
+color B1 
 timeout %break_interval% >NUL
 set /a randum=%random%
-REM start cmd /c "mode 57,7& echo off &cls & title The_Watcher&timeout 7 >NUL& del CSV.NET_ID.NETCONN.%randum%.CSV"
-
-echo. WScript.Sleep %helper_interval% > delete_me_%randum%.vbs
-
-echo. Set objFSO = CreateObject("Scripting.FileSystemObject") >> delete_me_%randum%.vbs
-echo.  If objFSO.FileExists("CSV.NET_ID.NETCONN.%randum%.CSV")  Then      >> delete_me_%randum%.vbs
-echo. objFSO.DeleteFile("CSV.NET_ID.NETCONN.%randum%.CSV") >> delete_me_%randum%.vbs
-echo. End if >> delete_me_%randum%.vbs
-echo.         objFSO.DeleteFile(Wscript.ScriptFullName) >> delete_me_%randum%.vbs
+REM start cmd /c "mode 57,7& echo off &cls & title The_Watcher&timeout 7 >NUL& del Delete_me_Csv.netcon.%randum%.CSV"
+set FILE_1=Delete_me_Csv.netcon.%randum%.CSV
+echo csvFileName = "%FILE_1%"  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo childScriptContent = _  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo "WScript.Sleep %helper_interval%" ^& vbCrLf ^& _  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo "Set objFSO = CreateObject(""Scripting.FileSystemObject"")" ^& vbCrLf ^& _  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo "If objFSO.FileExists(""" ^& csvFileName ^& """) Then" ^& vbCrLf ^& _  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo "   objFSO.DeleteFile(""" ^& csvFileName ^& """)" ^& vbCrLf ^& _  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo "End if" ^& vbCrLf ^& _  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo "objFSO.DeleteFile(WScript.ScriptFullName)"  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo childScriptFileName = "delete_me_%randum%.vbs"  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo Set objFSO = CreateObject("Scripting.FileSystemObject")  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo Set objFile = objFSO.CreateTextFile(childScriptFileName, True)  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo objFile.Write childScriptContent  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo objFile.Close  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo Set objShell = CreateObject("WScript.Shell")  >>PARENT_FAWNER_%RANDUM%.VBS 
+echo objShell.Run childScriptFileName, 0, False  >>PARENT_FAWNER_%RANDUM%.VBS 
+start cmd /c "echo off &title Running VBS script...& ( for /l %%i in (1,1,50) do type delete_me_%randum%.vbs ) &cls& color C2 & ( for /l %%i in (1,1,20) do echo ALL the JUNK Files ARE Going To BE Deleted!  )"
+wscript PARENT_FAWNER_%RANDUM%.VBS 
 rem mode 35,8 
 echo off
 title Watcher
-echo.Helper 
-color B1 
-wscript delete_me_%randum%.vbs 
+
 color F
 
-wmic nic get netconnectionid,netconnectionstatus,netenabled /format:CSV >> CSV.NET_ID.NETCONN.%randum%.CSV
-type CSV.NET_ID.NETCONN.%randum%.CSV | find /v "NetConnectionID,NetConnectionStatus,NetEnabled" >> CSV.NET_ID.NETCONN.%randum%2.CSV
+wmic nic get netconnectionid,netconnectionstatus,netenabled /format:CSV >> Delete_me_Csv.netcon.%randum%.CSV
+type Delete_me_Csv.netcon.%randum%.CSV | find /v "NetConnectionID,NetConnectionStatus,NetEnabled" >> Delete_me_Csv.netcon.%randum%2.CSV
 
 ECHO OFF
 CLS
-title Program: Network Manager
+title Enable Disable Network
 echo.**************************************************************
 echo. Disable/Enable
 echo.--------------------------------------------------------------
@@ -59,13 +78,14 @@ setlocal enabledelayedexpansion
 set list=
 set /a counter=0
 mode 40,20
-for /f "delims=" %%i in (CSV.NET_ID.NETCONN.%randum%2.CSV) do for /f "tokens=1,2,3,4 delims=," %%a in ("%%i") do if "%%b" NEQ "" set interface[!counter!]=%%b&set /a counter+=1
+for /f "delims=" %%i in (Delete_me_Csv.netcon.%randum%2.CSV) do for /f "tokens=1,2,3,4 delims=," %%a in ("%%i") do if "%%b" NEQ "" set interface[!counter!]=%%b&set /a counter+=1
 REM echo. There are !counter! results. Here are the names-
 set /a counter=counter-1
 for /l %%i in (0,1,!counter!) do set temp=!interface[%%i]!&set ptr[%%i]=!temp:^&amp^;=^&!
 
 for /l %%i in (0,1,!counter!) do echo %%i.!ptr[%%i]!
-del CSV.NET_ID.NETCONN.%randum%2.CSV
+del Delete_me_Csv.netcon.%randum%2.CSV
+del PArENT_FAWNER_%RANDUM%.VBS 
 
 :tryagain
 set /a x=1
@@ -89,6 +109,7 @@ if "%stat%"=="True" (Echo.) Else (Echo.Please Enter A Choice.&Goto tryagain)
 for /f %%i in ('powershell -c "$str=%delet%;write-host $str"') do set number=%%i
 if !number! LEQ !counter! (echo.GoodChoice 1>NUL) else (echo.Invalid Number&goto tryagain)
 set interface2day=!ptr[%number%]!
+title. Choose Enable or Disable
 echo.Chosen Interface !!interface2day!!
 CALL :again "!!interface2day!!"
 EndLocal
